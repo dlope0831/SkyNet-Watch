b7a0b0f9cc30706d602d68cc490cd83c67525649
Daniela Lopez <dlope0831@gmail.com>
2022-10-22 13:22:22 -0500

    added show button functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -107,19 +107,43 @@ router.get("/post/:id", (req, res) => {
         res.status(404).json({ message: "No post found with this id" })
         return
       }
-      //serialize the data
+      Likes.findOne({
+        where: {
+          post_id: req.params.id,
+          user_id: req.session.user_id
+        }
+      }) .then (likeData => {
+        //serialize the data
       const post = dbPostData.get({ plain: true })
-
+      // console.log(likeData != null)
+  
       //pass the data to template
       res.render("single-post", {
         post,
         loggedIn: req.session.loggedIn,
+        showButton: likeData === null
       })
     })
     .catch((err) => {
       console.log(err)
       res.status(500).json(err)
     })
+      })
+
+      // //serialize the data
+      // const post = dbPostData.get({ plain: true })
+      // console.log(dbPostData)
+      // console.log(req.session)
+      // //pass the data to template
+      // res.render("single-post", {
+      //   post,
+      //   loggedIn: req.session.loggedIn,
+      // })
+    // })
+    // .catch((err) => {
+    //   console.log(err)
+    //   res.status(500).json(err)
+    // })
 })
 
 module.exports = router
